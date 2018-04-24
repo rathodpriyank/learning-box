615198590b7f23b08928f3cd17d87879c69d2a46
Priyank Rathod <rathodpriyank@gmail.com>
2018-04-24 15:56:51 -0700

    Check endiness program

---
+++ gnu_functions/check_endiness.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdint.h>
+
+uint32_t ChangeEndianness(uint32_t u32Value)
+{
+	uint32_t u32Result = 0;
+	u32Result |= (u32Value & 0x000000FF) << 24;
+	u32Result |= (u32Value & 0x0000FF00) << 8;
+	u32Result |= (u32Value & 0x00FF0000) >> 8;
+	u32Result |= (u32Value & 0xFF000000) >> 24;
+	return u32Result;
+}
+
+int checkEndiness (int a) {
+    char *b = (char*) &a; 
+    if (*b) 
+       printf("Little \n");
+    else
+		printf("Big\n");
+}
+
+void main() {
+	int a = 10;
+	checkEndiness(a);
+	a = ChangeEndianness(a);
+	checkEndiness(a);
+
+}
+
